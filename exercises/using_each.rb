c84e3dd09c0b6cb7523dec576ce1aa4cef895ec3
Axel De La Guardia <axeldelaguardia@gmail.com>
2022-11-29 12:57:25 -0700

    Complete using_each.rb

--- exercises/using_each.rb
+++ exercises/using_each.rb
@@ -5,11 +5,16 @@
 
 students = ["Megan", "Bob", "Mike"]
 # your code below
-
+students.each do |student|
+    puts student
+end
 
 
 # Once you have your code written out:
 # What will the return value be:
 # - the first iteration through?
+# First iteration will print "Megan"
 # - the second iteration through?
+# Second iteration will print "Bob"
 # - the third iteration through?
+#  Third iteration will print "Mike"
